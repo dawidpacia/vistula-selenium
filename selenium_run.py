6c7438629062145d079261b4f2ab0a2811185d99
BRL021\brl021.krk <dawid.pacia@brainly.com>
2020-11-14 12:49:29 +0100

    selectors

--- selenium_run.py
+++ selenium_run.py
@@ -8,7 +8,13 @@ driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), optio
 
 driver.get("http://automationpractice.com/index.php")
 
-driver.find_element_by_id("search_query_top")
+driver.find_element_by_id("header_logo")
+driver.find_element_by_class_name("shopping_cart")
+driver.find_element_by_id("newsletter-input")
+driver.find_element_by_class_name("twitter")
+driver.find_element_by_class_name("homefeatured")
+driver.find_element_by_id("contact-link")
+
 
 driver.quit()
 
